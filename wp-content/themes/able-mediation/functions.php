ba870aae3ba4352bc5699440999c0557c66401a9
Martin Bagshaw <martinbagshaw1@gmail.com>
2017-05-09 14:06:43 +0100

    css and functions.php changes

--- wp-content/themes/able-mediation/functions.php
+++ wp-content/themes/able-mediation/functions.php
@@ -7,17 +7,17 @@ function blog_favicon() {
 	echo '<link rel="Shortcut Icon" type="image/x-icon" href="'.get_bloginfo('stylesheet_directory').'/images/able-icon.png" />' . "\n";
 	}
 add_action('wp_head', 'blog_favicon');
-	   
+
 // add google fonts //
 function load_fonts() {
 	wp_register_style('googleFonts', 'http://fonts.googleapis.com/css?family=Asap:700|Source+Sans+Pro:400,400italic,700', false, false, true);
 	wp_enqueue_style( 'googleFonts');
 	}
 add_action('wp_print_styles', 'load_fonts');
-		
+
 // add IE google fonts //
 function ie_fonts() {
-	echo "<!--[if IE]>
+	echo "<!--[if lt IE 9]>
 <link href='http://fonts.googleapis.com/css?family=Asap:700' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400italic' rel='stylesheet' type='text/css'>
@@ -40,14 +40,14 @@ function add_googleanalytics() { ?>
   //ga('send', 'pageview');
 
 </script>
-<?php } 
+<?php }
 
 // add secondary menu in the footer //
 register_nav_menus( array(
 'footernavigation' => __( 'Footer Navigation', 'ABLE Mediation' )
 ) );
 
-// add Stylesheets 
+// add Stylesheets
 function load_css() {
 	// ie css in header.php
 	//add print css
@@ -71,7 +71,7 @@ function jt_async_scripts($url){
     else if ( is_admin() )
         return str_replace( '#asyncload', '', $url );
     else
-	return str_replace( '#asyncload', '', $url )."' async='async"; 
+	return str_replace( '#asyncload', '', $url )."' async='async";
 }
 add_filter( 'clean_url', 'jt_async_scripts', 11, 1 );
 
@@ -79,13 +79,13 @@ add_filter( 'clean_url', 'jt_async_scripts', 11, 1 );
 // jQuery Scripts // true puts script in footer
 function load_scripts() {
 	if (!is_admin()) {
-	
+
 	// 1./ all pages
-	
+
 	// a) jquery ui plugin //
 	wp_register_script('jquery-ui', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js', 'jquery', '1.10.3', true);
 	wp_enqueue_script('jquery-ui');
-	
+
 	// b) cookies plugin //
 	if ( is_active_sidebar( 'cookies' ) ){
 		//wp_enqueue_script('jquery-cookie', get_stylesheet_directory_uri(). '/js/lib/jquery.cookie.js', array('jquery'), '1.0', true );
@@ -95,11 +95,11 @@ function load_scripts() {
 	}
 	// c) tooltip and cookies toggle //
 	wp_enqueue_script('tooltip', get_stylesheet_directory_uri(). '/js/tooltip.js#asyncload', array('jquery','jquery-ui'), '1.0', true);
-	
-	// main js didnt work, fucks sake: wp_enqueue_script('able-js', get_stylesheet_directory_uri(). '/js/able.js', array('jquery', 'jquery-ui', 'jquery-cookie' ), '1.0', true);
+
+	// main js didnt work - wp_enqueue_script('able-js', get_stylesheet_directory_uri(). '/js/able.js', array('jquery', 'jquery-ui', 'jquery-cookie' ), '1.0', true);
 
 
-// 2./ specific pages		
+// 2./ specific pages
 	// a) family mediation nav //
 	if (is_page_template('page-templates/family-mediation.php')){
 		wp_enqueue_script('fam-nav', get_stylesheet_directory_uri(). '/js/fam-nav.js#asyncload', array('jquery'), '1.0', true);
@@ -110,11 +110,11 @@ function load_scripts() {
 	}
 	// c) quote author styling and flexslider - home page //
 	if (is_page_template('page-templates/home-page.php')  || is_page_template('page-templates/home-page-slider.php')){
-		wp_enqueue_script('quote-author-home', get_stylesheet_directory_uri(). '/js/quote-author-home.js#asyncload', array('jquery'), '1.0' );
+		wp_enqueue_script('quote-author-home', get_stylesheet_directory_uri(). '/js/quote-author-home.js#asyncload', array('jquery'), '1.0', true );
 		// image slider
 		wp_register_script('flexslider', 'https://cdnjs.cloudflare.com/ajax/libs/flexslider/2.6.3/jquery.flexslider-min.js', 'jquery', '2.6.3', true);
 		wp_enqueue_script('flexslider');
-		wp_enqueue_script('flex-control', get_stylesheet_directory_uri(). '/js/flex-control.js#asyncload', array('jquery','flexslider'), '1.0', true);
+		wp_enqueue_script('flex-control', get_stylesheet_directory_uri(). '/js/flex-control.js', array('jquery','flexslider'), '1.0', true);
 	}
 	// d) quote author styling - all other pages //
 	if (!is_page_template('page-templates/home-page.php') && !is_page_template('our-mediators.php')){
@@ -127,11 +127,11 @@ function load_scripts() {
 
 	} // end customer side //
 }  // end load scripts //
-add_action('wp_enqueue_scripts', 'load_scripts');  
+add_action('wp_enqueue_scripts', 'load_scripts');
 
 
 
-// - - - - - - - - - 
+// - - - - - - - - -
 // use a cdn to load jquery instead
 function replace_jquery() {
 	if (!is_admin()) {
@@ -152,20 +152,20 @@ add_action('init', 'replace_jquery');
 // Remove Stuff //
 // ------------------ //
 // dequeue styles and scripts //
-function mytheme_dequeue_stuff() {
+function dequeue_stuff() {
 	wp_dequeue_style( 'twentytwelve-fonts' );
 	wp_dequeue_style( 'twentytwelve-ie' );
-	}	 
-add_action( 'wp_enqueue_scripts', 'mytheme_dequeue_stuff', 2 );
+	}
+add_action( 'wp_print_styles', 'dequeue_stuff' );
 
 
 // remove dashicons - render blocking above fold stuff
-function my_deregister_styles()    { 
+function deregister_styles()    {
    if (!is_admin() && !is_user_logged_in()) {
    	wp_deregister_style( 'dashicons' );
    }
 }
-add_action( 'wp_print_styles',     'my_deregister_styles', 100 );
+add_action( 'wp_print_styles', 'deregister_styles', 100 );
 
 
 // remove emojis
@@ -211,11 +211,11 @@ function remove_twentytwelve_options() {
 }
 // Remove top dashboard widget //
 if (is_admin(25)){
-function rc_my_welcome_panel() {	
+function rc_my_welcome_panel() {
 	?>
     <script type="text/javascript">
 /* Hide default welcome message */
-jQuery(document).ready( function($) 
+jQuery(document).ready( function($)
 {
 	$('div#welcome-panel').hide();
 });
@@ -264,7 +264,7 @@ add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin_style' );
 	   $contactmethods['twitter'] = '<p>d) Twitter Profile Link</br><em>Enter full url to display Twitter profile link.</p>' ;
 	   $contactmethods['googleplus'] = '<p>e) Google Plus Page Link</br><em>Enter full url to display Google Plus Page link.</em></p>' ;
 	   $contactmethods['linkedin'] = '<p>f) Linkedin Profile Link</br><em>Enter full url to display Linkedin profile link.</p>' ;
-	   $contactmethods['companyname'] = '<p>g) Company Name</br><em>Enter to display in the footer area</em></p>';	   
+	   $contactmethods['companyname'] = '<p>g) Company Name</br><em>Enter to display in the footer area</em></p>';
        $contactmethods['companynumber'] = '<p>h) Registered Company Number</br><em>Enter to display in the footer area</em></p>';
 	   $contactmethods['registeredaddress'] = '<p>i) Registered Company Address</br><em>Enter to display in the footer area</em></p>';
 // referral
@@ -324,17 +324,17 @@ add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin_style' );
        add_filter('user_contactmethods','my_new_contactmethods',10,1);
 
 
-	   
+
 function footerlogo_js() {
 ?><script type="text/javascript">
 	jQuery(document).ready(function() {
-	
+
 	// logo 1
 	jQuery(document).find("input[id^='uploadfooterlogo1']").live('click', function(){
 	//var num = this.id.split('-')[1];
 	formfield = jQuery('#footerlogo1').attr('name');
 	tb_show('', 'media-upload.php?type=image&amp;TB_iframe=true');
- 
+
 	window.send_to_editor = function(html) {
 	imgurl = jQuery('img',html).attr('src');
 	jQuery('#footerlogo1').val(imgurl);
@@ -342,12 +342,12 @@ function footerlogo_js() {
 	}
 	return false;
 	});
-	
+
 	// logo 2
 	jQuery(document).find("input[id^='uploadfooterlogo2']").live('click', function(){
 	formfield = jQuery('#footerlogo2').attr('name');
 	tb_show('', 'media-upload.php?type=image&amp;TB_iframe=true');
- 
+
 	window.send_to_editor = function(html) {
 	imgurl = jQuery('img',html).attr('src');
 	jQuery('#footerlogo2').val(imgurl);
@@ -355,12 +355,12 @@ function footerlogo_js() {
 	}
 	return false;
 	});
-	
+
 	// logo 3
 	jQuery(document).find("input[id^='uploadfooterlogo3']").live('click', function(){
 	formfield = jQuery('#footerlogo3').attr('name');
 	tb_show('', 'media-upload.php?type=image&amp;TB_iframe=true');
- 
+
 	window.send_to_editor = function(html) {
 	imgurl = jQuery('img',html).attr('src');
 	jQuery('#footerlogo3').val(imgurl);
@@ -368,12 +368,12 @@ function footerlogo_js() {
 	}
 	return false;
 	});
-	
+
 	// logo 4
 	jQuery(document).find("input[id^='uploadfooterlogo4']").live('click', function(){
 	formfield = jQuery('#footerlogo4').attr('name');
 	tb_show('', 'media-upload.php?type=image&amp;TB_iframe=true');
- 
+
 	window.send_to_editor = function(html) {
 	imgurl = jQuery('img',html).attr('src');
 	jQuery('#footerlogo4').val(imgurl);
@@ -381,12 +381,12 @@ function footerlogo_js() {
 	}
 	return false;
 	});
-	
+
 	// logo 5
 	jQuery(document).find("input[id^='uploadfooterlogo5']").live('click', function(){
 	formfield = jQuery('#footerlogo5').attr('name');
 	tb_show('', 'media-upload.php?type=image&amp;TB_iframe=true');
- 
+
 	window.send_to_editor = function(html) {
 	imgurl = jQuery('img',html).attr('src');
 	jQuery('#footerlogo5').val(imgurl);
@@ -394,7 +394,7 @@ function footerlogo_js() {
 	}
 	return false;
 	});
-	
+
 	// ----------------------------------
 
 	// conditional image display in editor
@@ -402,25 +402,25 @@ function footerlogo_js() {
 	if (jQuery("input[id^='footerlogo1']").val()) {
     	jQuery('.footerlogo1').attr('src', logo1);
 	}
-	
+
 	// conditional image display in editor
 	logo2 = jQuery("input[id^='footerlogo2']").val()
 	if (jQuery("input[id^='footerlogo2']").val()) {
     	jQuery('.footerlogo2').attr('src', logo2);
 	}
-	
+
 	// conditional image display in editor
 	logo3 = jQuery("input[id^='footerlogo3']").val()
 	if (jQuery("input[id^='footerlogo3']").val()) {
     	jQuery('.footerlogo3').attr('src', logo3);
 	}
-	
+
 	// conditional image display in editor
 	logo4 = jQuery("input[id^='footerlogo4']").val()
 	if (jQuery("input[id^='footerlogo4']").val()) {
     	jQuery('.footerlogo4').attr('src', logo4);
 	}
-	
+
 	// conditional image display in editor
 	logo5 = jQuery("input[id^='footerlogo5']").val()
 	if (jQuery("input[id^='footerlogo5']").val()) {
@@ -440,10 +440,10 @@ add_action( 'user_contactmethods', $contactmethods['footerlogo1'] );
 
 
 
-	   
-	   
-	   
-	   
+
+
+
+
 // --------------------- //
 // Add Mediators Profile Image
 add_action( 'show_user_profile', 'my_show_extra_profile_fields' );
@@ -482,13 +482,13 @@ function zkr_profile_upload_js() {
 	//var num = this.id.split('-')[1];
 	formfield = jQuery('#image').attr('name');
 	tb_show('', 'media-upload.php?type=image&amp;TB_iframe=true');
- 
+
 	window.send_to_editor = function(html) {
 	imgurl = jQuery('img',html).attr('src');
 	jQuery('#image').val(imgurl);
 	tb_remove();
 	}
- 
+
 return false;
 });
 });
@@ -497,14 +497,14 @@ return false;
 }
 add_action('admin_head','zkr_profile_upload_js');
 // add media upload styles and scripts if the user is logged in
-if (is_user_logged_in()) {
+if (is_admin() && is_user_logged_in()) {
 	wp_enqueue_script('media-upload');
 	wp_enqueue_script('thickbox');
 	wp_enqueue_style('thickbox');
 }
 add_action( 'personal_options_update', 'my_save_extra_profile_fields' );
 add_action( 'edit_user_profile_update', 'my_save_extra_profile_fields' );
- 
+
 function my_save_extra_profile_fields( $user_id ) {
 	 if ( !current_user_can( 'edit_user', $user_id ) )
 	return false;
@@ -515,7 +515,7 @@ function my_save_extra_profile_fields( $user_id ) {
 
 
 
-// remove default sidebars and widgets //  
+// remove default sidebars and widgets //
 function remove_some_widgets(){
     unregister_sidebar( 'sidebar-1' );
     unregister_sidebar( 'sidebar-2' );
@@ -535,7 +535,7 @@ function remove_some_widgets(){
 	// unregister_widget( 'WP_Widget_Categories' );
 }
 add_action( 'widgets_init', 'remove_some_widgets', 11 );
- 
+
  // register sidebars for different pages and blog //
 if ( function_exists('register_sidebar') )
     register_sidebar( array(
@@ -547,7 +547,7 @@ if ( function_exists('register_sidebar') )
    'before_title' => '<h3 class="widget-title">',
    'after_title' => '</h3>',
    ) );
-   
+
    register_sidebar( array(
    'name' => __( 'Other Pages'),
    'id' => 'testimonialcontact',
@@ -557,7 +557,7 @@ if ( function_exists('register_sidebar') )
    'before_title' => '<h3 class="widget-title">',
    'after_title' => '</h3>',
    ) );
-   
+
    register_sidebar( array(
    'name' => __( 'Blog Pages'),
    'id' => 'blogsidebar',
@@ -567,7 +567,7 @@ if ( function_exists('register_sidebar') )
    'before_title' => '<h3 class="widget-title">',
    'after_title' => '</h3>',
    ) );
-   
+
    // Cookies Widget Area in Header //
 if ( function_exists('register_sidebar') )
     register_sidebar( array(
@@ -577,20 +577,20 @@ if ( function_exists('register_sidebar') )
    'before_widget' => '<div class="cookies">',
    'after_widget' => '</div>',
    ) );
-   
-   
-   
+
+
+
 // Custom Cookies Widget  //
 class analytics_widget extends WP_Widget {
 
 function __construct() {
 	parent::__construct(
 	// Base ID of your widget
-	'analytics_widget', 
+	'analytics_widget',
 	// Widget name will appear in UI
-	__('Google Analytics Cookies Notice', 'analytics_widget_domain'), 
+	__('Google Analytics Cookies Notice', 'analytics_widget_domain'),
 	// Widget description
-	array( 'description' => __( 'Add your Cookies Notice here', 'analytics_widget_domain' ), ) 
+	array( 'description' => __( 'Add your Cookies Notice here', 'analytics_widget_domain' ), )
 	);
 }
 
@@ -603,14 +603,14 @@ public function widget( $args, $instance ) {
 	echo $args['before_widget'];
 		if ( ! empty( $content ) )
 		echo $args['before_content'] .'
-			<div class="textwidget">'. $content .' <a href="'. $link .'">Privacy and Cookie Policy Page</a> <div class="hide">Hide this notice ×</div></div>'. 
+			<div class="textwidget">'. $content .' <a href="'. $link .'">Privacy and Cookie Policy Page</a> <div class="hide">Hide this notice ×</div></div>'.
 			$args['after_content'];
 		// This is where you run the code and display the output
 	echo $args['after_widget'];
 }
 
-		
-// Widget Backend 
+
+// Widget Backend
 public function form( $instance ) {
 	if ( isset( $instance[ 'content' ] ) ) {
 	$content = $instance[ 'content' ];
@@ -630,12 +630,12 @@ public function form( $instance ) {
 <p>
 <label for="<?php echo $this->get_field_id( 'content' ); ?>"><?php _e( 'Content:' ); ?></label>
 <textarea class="widefat" id="<?php echo $this->get_field_id( 'content' ); ?>" name="<?php echo $this->get_field_name( 'content' ); ?>" cols="20" rows="5"><?php echo esc_attr( $content ); ?></textarea>
-<label for="<?php echo $this->get_field_id( 'link' ); ?>"><?php _e( 'Link to Privacy Policy Page:' ); ?></label> 
+<label for="<?php echo $this->get_field_id( 'link' ); ?>"><?php _e( 'Link to Privacy Policy Page:' ); ?></label>
 <input class="widefat" id="<?php echo $this->get_field_id( 'link' ); ?>" name="<?php echo $this->get_field_name( 'link' ); ?>" type="text" value="<?php echo esc_attr( $link ); ?>" />
 </p>
-<?php 
+<?php
 }
-	
+
 // Updating widget replacing old instances with new
 public function update( $new_instance, $old_instance ) {
 	$instance = array();
@@ -649,7 +649,7 @@ return $instance;
 function analytics_load_widget() {
 	register_widget( 'analytics_widget' );
 }
-add_action( 'widgets_init', 'analytics_load_widget' ); 
+add_action( 'widgets_init', 'analytics_load_widget' );
 
 
 
@@ -668,13 +668,13 @@ function google_maps() {
 	if (is_page_template('page-templates/contact.php')){
 		echo '<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDApA44NzVrUmgronW53dRcmNGvJsiiepY&amp;sensor=false" type="text/javascript"></script>';
 		// wp_enqueue_script('brentford', get_stylesheet_directory_uri(). '/js/contact/brentford.js');
-		
+
 		wp_enqueue_script('ealing', get_stylesheet_directory_uri(). '/js/contact/ealing.js#asyncload', '', '1.0', true);
   		wp_enqueue_script('richmond', get_stylesheet_directory_uri(). '/js/contact/richmond.js#asyncload', '', '1.0', true);
   		wp_enqueue_script('central-london', get_stylesheet_directory_uri(). '/js/contact/central-london.js#asyncload', '', '1.0', true);
   		wp_enqueue_script('northfields', get_stylesheet_directory_uri(). '/js/contact/northfields.js#asyncload', '', '1.0', true);
 		wp_enqueue_script('hammersmith', get_stylesheet_directory_uri(). '/js/contact/hammersmith.js#asyncload', '', '1.0', true);
-		
+
 		//wp_enqueue_script('able-maps', get_stylesheet_directory_uri(). '/js/able-maps.min.js'); - future attempt to combine maps
 		// http://stackoverflow.com/questions/4074520/how-to-display-multiple-google-maps-per-page-with-api-v3
 	 }
@@ -700,12 +700,12 @@ function my_login_logo() { ?>
 		#loginform input[type="submit"]:hover,
 		#loginform input[type="submit"]:focus,
 		#loginform input[type="submit"]:active {background-color:rgba(119, 115, 115, 1);color:white;}
-		.login #nav a, .login #backtoblog a {text-decoration:none;color:rgba(112, 191, 68, 1) !important;}	
+		.login #nav a, .login #backtoblog a {text-decoration:none;color:rgba(112, 191, 68, 1) !important;}
     </style>
 <?php }
 	wp_register_style('googleFonts', 'http://fonts.googleapis.com/css?family=Asap:700|Source+Sans+Pro:400,400italic,700');
 	wp_enqueue_style( 'googleFonts');
-	
+
 add_action( 'login_enqueue_scripts', 'my_login_logo' );
 function my_login_logo_url() {
     return get_bloginfo( 'url' );
@@ -723,10 +723,10 @@ add_filter( 'login_headertitle', 'my_login_logo_url_title' );
 
 // add custom dashboard widget at the top of the dashboard //
 function example_dashboard_widget_function() {
-	$user = wp_get_current_user();	
-	if($user->user_level < 10) :	
-    echo "Hello <strong>" . $user->user_login . "</strong>, and welcome to the Able mediation website. Below is a link to the old <strong>Author Guide</strong>, which will show you how to publish <strong>Blog Posts</strong> which include links, images, and documents, and update your User Profile.</br></br>"."<a href='http://localhost/ABLE-mediation/wp-content/uploads/2013/08/author-guide.pdf'>"."Able Mediation Author User Guide"."</a>"."</br>"; 
-	elseif($user->user_level > 9) : 
+	$user = wp_get_current_user();
+	if($user->user_level < 10) :
+    echo "Hello <strong>" . $user->user_login . "</strong>, and welcome to the Able mediation website. Below is a link to the old <strong>Author Guide</strong>, which will show you how to publish <strong>Blog Posts</strong> which include links, images, and documents, and update your User Profile.</br></br>"."<a href='http://localhost/ABLE-mediation/wp-content/uploads/2013/08/author-guide.pdf'>"."Able Mediation Author User Guide"."</a>"."</br>";
+	elseif($user->user_level > 9) :
 	echo 'Hello <strong>' . $user->user_login . '</strong>, and welcome to the Able mediation website. Below is a link to the old <strong>Website Administrator Guide</strong>, which will show you how to edit the website, though some parts will no longer apply. </br></br><a href="http://localhost/ABLE-mediation/wp-content/uploads/2013/08/admin-guide.pdf">Able Mediation Website Administrator Guide</a></br></br>The new guide will cover the below topics:</br>
 	<ol class="mainlist">
 		<li><h3 class="guide greenspan">Plugins (installed by developer)</h3>
@@ -768,8 +768,8 @@ function example_add_dashboard_widgets() {
 	$example_widget_backup = array('example_dashboard_widget' => $normal_dashboard['example_dashboard_widget']);
 	unset($normal_dashboard['example_dashboard_widget']);
 	$sorted_dashboard = array_merge($example_widget_backup, $normal_dashboard);
-	$wp_meta_boxes['dashboard']['normal']['core'] = $sorted_dashboard;	
-} 
+	$wp_meta_boxes['dashboard']['normal']['core'] = $sorted_dashboard;
+}
 add_action('wp_dashboard_setup', 'example_add_dashboard_widgets' );
 
 
