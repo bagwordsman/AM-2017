592cd32ffdba89efcb27c4f8e4fe7df77609072f
Martin Bagshaw <martinbagshaw1@gmail.com>
2017-05-13 20:16:35 +0100

    update functions to include mediators page content parsing - enabled through visual biography editor function

--- wp-content/themes/able-mediation/functions.php
+++ wp-content/themes/able-mediation/functions.php
@@ -273,7 +273,90 @@ add_action( 'admin_enqueue_scripts', 'admin_css_js' );
 
 
 
+// ----------------------- //
+// edit user biography with TinyMCE visual editor
+class KLVisualBiographyEditor {
+	private $name = 'Visual Biography Editor';
 
+	/**
+	 * Setup WP hooks
+	 */
+	public function __construct() {
+		// Add a visual editor if the current user is an Author role or above and WordPress is v3.3+
+		if ( function_exists('wp_editor') ) {
+
+			// Add the WP_Editor
+			add_action( 'show_user_profile', array($this, 'visual_editor') );
+			add_action( 'edit_user_profile', array($this, 'visual_editor') );
+
+			// Don't sanitize the data for display in a textarea
+			add_action( 'admin_init', array($this, 'save_filters') );
+
+			// Load required JS
+			add_action( 'admin_enqueue_scripts', array($this, 'load_javascript'), 10, 1 );
+
+			// Add content filters to the output of the description
+			add_filter( 'get_the_author_description', 'wptexturize' );
+			add_filter( 'get_the_author_description', 'convert_chars' );
+			add_filter( 'get_the_author_description', 'wpautop' );
+		}
+		// Display a message if the requires aren't met
+		else {
+			add_action( 'admin_notices', array($this, 'update_notice') );
+		}
+	}
+
+	/**
+	 * Friendly notice if WP is out of date
+	 */
+	public function update_notice() {
+
+		// Notification is for administrators only
+		if ( !current_user_can('administrator') )
+			return;
+
+		?>
+		<div class="updated">
+			<p>The <strong><?php echo $this->name; ?></strong> plugin requires WordPress 3.3 or higher. Update WordPress or <a href="<?php echo get_admin_url(null, 'plugins.php'); ?>">de-activate the plugin</a>.</p>
+		</div>
+		<?php
+	}
+
+	/**
+	 *	Create Visual Editor - Add TinyMCE editor to replace the "Biographical Info" field in a user profile
+	 */
+	public function visual_editor( $user ) {
+
+		// Contributor level user or higher required
+		if ( !current_user_can('edit_posts') )
+			return;
+		?>
+		<table class="form-table">
+			<tr>
+				<th><label for="description"><?php _e('Biographical Info'); ?></label></th>
+				<td>
+					<?php
+					$description = get_user_meta( $user->ID, 'description', true);
+					wp_editor( $description, 'description' );
+					?>
+					<p class="description"><?php _e('Share a little biographical information to fill out your profile. This may be shown publicly.'); ?></p>
+				</td>
+			</tr>
+		</table>
+		<?php
+	}
+	/**
+	 * Remove textarea filters from description field
+	 */
+	public function save_filters() {
+		// Contributor level user or higher required
+		if ( !current_user_can('edit_posts') )
+			return;
+		remove_all_filters('pre_user_description');
+	}
+}
+
+$kpl_visual_editor_biography = new KLVisualBiographyEditor();
 
 
 
