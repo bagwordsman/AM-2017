48c84f1c2fae9e6548ba551f12527bc328425502
Martin Bagshaw <martinbagshaw1@gmail.com>
2018-10-11 16:39:40 +0100

    css, js, img optimisation

--- wp-content/themes/AM-2017/includes/client-side/css-js.php
+++ wp-content/themes/AM-2017/includes/client-side/css-js.php
@@ -10,13 +10,19 @@
 // 1 - css
 function load_css() {
 	// style.css
-	//  - required by theme, resides in the root, acts as a reset
-	wp_register_style('reset', get_stylesheet_directory_uri() . '/style.css' );
-	wp_enqueue_style( 'reset');
+	// - combined reset followed by theme styles
+	wp_register_style('main_css', get_stylesheet_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'main_css');
 
-	// complied css for all pages
-	wp_register_style( 'compiled-css', get_stylesheet_directory_uri(). '/css/style.css' );
-	wp_enqueue_style('compiled-css' );
+
+	// _______________
+	//  - required by theme, resides in the root, acts as a reset
+	// wp_register_style('reset', get_stylesheet_directory_uri() . '/style.css' );
+	// wp_enqueue_style( 'reset');
+
+	// // complied css for all pages
+	// wp_register_style( 'compiled-css', get_stylesheet_directory_uri(). '/css/style.css' );
+	// wp_enqueue_style('compiled-css' );
 
 	// add print css
 	// wp_register_style('print', get_stylesheet_directory_uri() . '/css/print/AM2017-print.css', false, false, 'print');
@@ -35,18 +41,51 @@ add_action('wp_enqueue_scripts', 'load_css');
 function load_scripts() {
 	if (!is_admin()) {
 		
+		// _______________
+		// add jQuery from CDN for production
+		wp_deregister_script('jquery');
+		wp_register_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js', null, null, true );
+		wp_enqueue_script('jquery');
+
+		// _______________
 		// lib > jquery-ui
-		wp_enqueue_script('jquery-ui', get_stylesheet_directory_uri(). '/js/lib/jquery-ui.min.js', array('jquery'), '1.0', true );
+		// wp_enqueue_script('jquery-ui', get_stylesheet_directory_uri(). '/js/lib/jquery-ui.min.js', array('jquery'), '1.0', true );
+		
+		// jQuery UI CDN (for live):		
+		wp_enqueue_script('jquery-ui', 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js', null, null, true );
+		
+		// _______________
 		// lib > cookies
-		wp_enqueue_script('jquery-cookie', get_stylesheet_directory_uri(). '/js/lib/jquery.cookie.js', array('jquery'), '1.0', true  );
-		// lib > twitter
-		wp_enqueue_script('twitter', get_stylesheet_directory_uri(). '/js/lib/twitterfeed.js', array('jquery'), '1.0', true  );
-		// main js
-		wp_enqueue_script('main', get_stylesheet_directory_uri(). '/js/main.js', array('jquery','jquery-cookie'), '1.0', true  ); // 'jquery-ui'
+		// wp_enqueue_script('jquery-cookie', get_stylesheet_directory_uri(). '/js/lib/jquery.cookie.js', array('jquery'), '1.0', true  );
+		// // lib > twitter
+		// wp_enqueue_script('twitter', get_stylesheet_directory_uri(). '/js/lib/twitterfeed.js', array('jquery'), '1.0', true  );
+		// // main js
+		// wp_enqueue_script('main', get_stylesheet_directory_uri(). '/js/main.js', array('jquery','jquery-cookie'), '1.0', true  );
+
+		// theme JS - combined and minified the above 3 scripts (for live):
+		wp_enqueue_script('main_bundle', get_stylesheet_directory_uri(). '/js/main_bundle.js', array('jquery'), '1.0', true );
 
 	}
 }
 add_action('wp_enqueue_scripts', 'load_scripts');
+
+
+// defer loading of all scripts
+function js_async_attr($tag){
+	
+	// exclude some scripts from deferral by adding their tags here:
+	$scripts_to_exclude = array('https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js');
+
+	foreach($scripts_to_exclude as $exclude_script) {
+		if(true == strpos($tag, $exclude_script ) )
+		return $tag;
+	}
+
+	// defer or async all remaining scripts not excluded above
+	return str_replace( ' src', ' defer="defer" src', $tag );
+}
+add_filter( 'script_loader_tag', 'js_async_attr', 10 );
+
 
 
 
@@ -65,7 +104,10 @@ if ($lazyload_on != '') {
 		wp_enqueue_script('jquery_lazy_load', get_stylesheet_directory_uri() . '/js/lib/jquery.lazyload.min.js', array('jquery'), '1.0');
 	}
 	add_action('wp_enqueue_scripts', 'enqueue_lazyload');
-	// add lazyload class to .page - in header.php. This allows script to be fired from main.js
+	// add lazyload class to .page - in header.php.
+	// - this could allow script to be fired from main.js or main_bundle.js if a class is present
+	// - note: may not have worked previously because main_bundle is loaded in the footer, and lazyload in the header
+	// - also because script needs to fire to prevent the DOM from fully loading
 
 
 	// filter the_content and acf_the_content
